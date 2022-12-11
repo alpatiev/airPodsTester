1010fb1d3a6b9f05971577855b308132f3f96121
AlpatyevNikita <chococacaococonut@gmail.com>
2022-12-11 14:46:04 +0300

    added empty project, removed storyboard, , made loginViewController as root

---
+++ airPodsTester/airPodsTester/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  airPodsTester
+//
+//  Created by Nikita Alpatiev on 12/11/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
+
---
+++ airPodsTester/airPodsTester/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  airPodsTester
+//
+//  Created by Nikita Alpatiev on 12/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+    }
+}
+
